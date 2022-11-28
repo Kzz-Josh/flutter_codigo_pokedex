70ca586c49807d23c8d9c97eb98c90cb2ef0b682
Josh <201913147g@utea.edu.pe>
2022-11-28 15:04:23 -0500

    libreria http

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  //Uri
+
+  getDataPokemon() async {
+    Uri _uri = Uri.parse(
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
+    http.Response response = await http.get(_uri);
+    print(response.statusCode);
+    print(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Data"),
+          onPressed: () {
+            getDataPokemon();
+          },
+        ),
+      ),
+    );
   }
 }
