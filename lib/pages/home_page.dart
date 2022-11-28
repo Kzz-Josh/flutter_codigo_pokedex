75b6d36722ec1654a7fbd3c130486adb0e41530a
Josh <201913147g@utea.edu.pe>
2022-11-28 17:05:58 -0500

    Mostrar Datos en UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,23 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_pokedex/ui/widgets/item_pokemon_widget.dart';
 import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   //Uri
   List pokemons = [];
+
+  @override
+  initState() {
+    super.initState();
+    getDataPokemon();
+  }
+
   getDataPokemon() async {
     Uri _uri = Uri.parse(
         "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
@@ -12,13 +25,15 @@ class HomePage extends StatelessWidget {
     if (response.statusCode == 200) {
       Map<String, dynamic> myMap = json.decode(response.body);
       pokemons = myMap["pokemon"];
-      print(pokemons);
+      setState(() {});
+      // pokemons.forEach((element) {
+      //   print(element["name"]);
+      // });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    getDataPokemon();
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -41,79 +56,21 @@ class HomePage extends StatelessWidget {
                   height: 30.0,
                 ),
                 GridView.count(
+                  physics: const ScrollPhysics(),
                   shrinkWrap: true,
                   crossAxisCount: 2,
                   mainAxisSpacing: 12.0,
                   crossAxisSpacing: 12.0,
                   childAspectRatio: 1.35,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Color(0Xff4CCFB2),
-                        borderRadius: BorderRadius.circular(18.0),
-                      ),
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            bottom: -20,
-                            right: -20,
-                            child: Image.asset(
-                              'assets/images/pokeball.png',
-                              height: 120,
-                              color: Colors.white.withOpacity(0.27),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 20.0, horizontal: 12.0),
-                            child: Column(
-                              children: [
-                                Text(
-                                  "Bulbasaur",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Container(
-                                  margin:
-                                      const EdgeInsets.symmetric(vertical: 6.0),
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 14, vertical: 4.0),
-                                  decoration: BoxDecoration(
-                                    color: Colors.white.withOpacity(0.27),
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.black.withOpacity(0.04),
-                                        offset: const Offset(4, 4),
-                                        blurRadius: 12.0,
-                                      )
-                                    ],
-                                  ),
-                                  child: Text(
-                                    "Grass",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            right: 0,
-                            child: Image.network(
-                              "http://www.serebii.net/pokemongo/pokemon/001.png",
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                )
+                  children: pokemons
+                      .map(
+                        (e) => ItemPokemonWidget(
+                          name: e["name"],
+                          image: e["img"],
+                        ),
+                      )
+                      .toList(),
+                ),
               ],
             ),
           ),
