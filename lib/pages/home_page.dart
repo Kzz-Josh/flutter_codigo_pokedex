f598587193ebe71abbc8d867f4ec38c76bccb30d
Josh <201913147g@utea.edu.pe>
2022-11-28 18:00:57 -0500

    Mod metodo de consumo APIs...

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_pokedex/models/pokemon_model.dart';
 import 'package:flutter_codigo_pokedex/ui/widgets/item_pokemon_widget.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,6 +12,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   //Uri
   List pokemons = [];
+  List<PokemonModel> pokemonsModel = [];
 
   @override
   initState() {
@@ -24,7 +26,11 @@ class _HomePageState extends State<HomePage> {
     http.Response response = await http.get(_uri);
     if (response.statusCode == 200) {
       Map<String, dynamic> myMap = json.decode(response.body);
-      pokemons = myMap["pokemon"];
+      //pokemons = myMap["pokemon"];
+      pokemonsModel = myMap["pokemon"]
+          .map<PokemonModel>((e) => PokemonModel.fromJson(e))
+          .toList();
+      print(pokemonsModel);
       setState(() {});
       // pokemons.forEach((element) {
       //   print(element["name"]);
@@ -62,13 +68,12 @@ class _HomePageState extends State<HomePage> {
                   mainAxisSpacing: 12.0,
                   crossAxisSpacing: 12.0,
                   childAspectRatio: 1.35,
-                  children: pokemons
+                  children: pokemonsModel
                       .map(
                         (e) => ItemPokemonWidget(
-                          name: e["name"],
-                          image: e["img"],
-                          types:
-                              List<String>.from(e["type"].map((item) => item)),
+                          name: e.name,
+                          image: e.img,
+                          types: e.type,
                         ),
                       )
                       .toList(),
